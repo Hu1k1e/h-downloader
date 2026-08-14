b955c01cf83f4b65150dc8d30e0204b33492f233
H-Downloader <svija@h-downloader.local>
2026-08-13 21:50:24 -0400

    chore: remove test files

--- extract_links.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('fmovies.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-links = set(re.findall(r'href=[\"\'](.*?)[\"\']', text))
-with open('links.txt', 'w', encoding='utf-8') as f:
-    for link in sorted(links):
-        f.write(link + '\n')
